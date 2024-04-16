d38cb4bf1dbe398d5c5edcb1c90adcb496de0064
Pilzinsel64 <pilzinsel64@gmx.de>
2024-04-16 14:31:46 +0200

    init

---
+++ src/main/java/de/pilz/alternativechunkloading/AlternativeChunkloading.java
@@ -0,0 +1,41 @@
+package com.myname.mymodid;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = MyMod.MODID, version = Tags.VERSION, name = "Alternative Chunkloading", acceptedMinecraftVersions = "[1.7.10]")
+public class MyMod {
+
+    public static final String MODID = "alternativechunkloading";
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    @SidedProxy(clientSide = "de.pilz.alternativechunkloading.ClientProxy", serverSide = "de.pilz.alternativechunkloading.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+}
