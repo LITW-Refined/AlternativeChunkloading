f4c46cde676ae40da1a1d5f66d2a561814813e66
Pilzinsel64 <pilzinsel64@gmx.de>
2024-12-25 09:14:55 +0100

    switch to @WrapOperation annotation - improves possible compatibility with other mods - also wrap `isAirBlock` in `placeInExistingPortal`

--- src/main/java/de/pilz/alternativechunkloading/mixin/early/MixinTeleporter.java
+++ src/main/java/de/pilz/alternativechunkloading/mixin/early/MixinTeleporter.java
@@ -6,58 +6,72 @@ import net.minecraft.world.WorldServer;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import de.pilz.alternativechunkloading.Utils;
 
 @Mixin(Teleporter.class)
 public class MixinTeleporter {
 
-    @Redirect(
+    @WrapOperation(
         method = "placeInExistingPortal",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/WorldServer;getBlock(III)Lnet/minecraft/block/Block;"))
-    private Block pilz$placeInExistingPortal$getBlock(WorldServer world, int x, int y, int z) {
+    private Block pilz$placeInExistingPortal$getBlock(WorldServer world, int x, int y, int z,
+        Operation<Block> original) {
         Utils.ensureChunkLoaded(world, x, y, z);
-        return world.getBlock(x, y, z);
+        return original.call(world, x, y, z);
     }
 
-    @Redirect(
+    @WrapOperation(
+        method = "placeInExistingPortal",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldServer;isAirBlock(III)Z"))
+    private boolean pilz$placeInExistingPortal$isAirBlock(WorldServer world, int x, int y, int z,
+        Operation<Boolean> original) {
+        Utils.ensureChunkLoaded(world, x, y, z);
+        return original.call(world, x, y, z);
+    }
+
+    @WrapOperation(
         method = "placeInPortal",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/WorldServer;setBlock(IIILnet/minecraft/block/Block;)Z"))
-    private boolean pilz$placeInPortal$setBlock(WorldServer world, int x, int y, int z, Block block) {
+    private boolean pilz$placeInPortal$setBlock(WorldServer world, int x, int y, int z, Block block,
+        Operation<Boolean> original) {
         Utils.ensureChunkLoaded(world, x, y, z);
-        return world.setBlock(x, y, z, block);
+        return original.call(world, x, y, z, block);
     }
 
-    @Redirect(
+    @WrapOperation(
         method = "makePortal",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/WorldServer;getBlock(III)Lnet/minecraft/block/Block;"))
-    private Block pilz$makePortal$getBlock(WorldServer world, int x, int y, int z) {
+    private Block pilz$makePortal$getBlock(WorldServer world, int x, int y, int z, Operation<Block> original) {
         Utils.ensureChunkLoaded(world, x, y, z);
-        return world.getBlock(x, y, z);
+        return original.call(world, x, y, z);
     }
 
-    @Redirect(
+    @WrapOperation(
         method = "makePortal",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/WorldServer;setBlock(IIILnet/minecraft/block/Block;)Z"))
-    private boolean pilz$makePortal$setBlock(WorldServer world, int x, int y, int z, Block block) {
+    private boolean pilz$makePortal$setBlock(WorldServer world, int x, int y, int z, Block block,
+        Operation<Boolean> original) {
         Utils.ensureChunkLoaded(world, x, y, z);
-        return world.setBlock(x, y, z, block);
+        return original.call(world, x, y, z, block);
     }
 
-    @Redirect(
+    @WrapOperation(
         method = "makePortal",
         at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldServer;isAirBlock(III)Z"))
-    private boolean pilz$makePortal$isAirBlock(WorldServer world, int x, int y, int z) {
+    private boolean pilz$makePortal$isAirBlock(WorldServer world, int x, int y, int z, Operation<Boolean> original) {
         Utils.ensureChunkLoaded(world, x, y, z);
-        return world.isAirBlock(x, y, z);
+        return original.call(world, x, y, z);
     }
 }
